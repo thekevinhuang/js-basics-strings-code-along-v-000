8062029b4f096f13f02e3a800ca73eb904914b62
Kevin Huang <yellow.khaos@gmail.com>
2018-09-30 01:27:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
+let shortGreeting = "Welcome, $(currentUser.slice(0,1))!"
