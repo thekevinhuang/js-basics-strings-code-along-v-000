bca096908382a60cf98ad9b56f00ea44b7407c3e
Kevin Huang <yellow.khaos@gmail.com>
2018-09-30 01:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ let currentUser = "me"
 
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
